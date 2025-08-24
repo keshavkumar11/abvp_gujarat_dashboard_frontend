3066baf0c88870aa8ecee29fcb50a15495afd2f9
keshavkumar11 <keshavkumarpurani@gmail.com>
2025-08-24 20:24:38 +0530

    feat(frontend): setup project structure and install dependencies

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    <>
+    <div className='flex items-center justify-center h-screen bg-bluw-100'>
+      <h1 className='text-3xl font-bold text-blue-800'>
+        ABVP MEMBERSHIP PORTAL
+      </h1>
+    </div>
+    </>
+  );
+}
+
+export default App;
